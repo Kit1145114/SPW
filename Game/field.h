6dfc29830d61a1fd95bb4a4e739a2d36b3c4f48f
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2019-02-14 15:29:13 +0900

    field実装

---
+++ Game/field.h
@@ -0,0 +1,17 @@
+#pragma once
+class field : public IGameObject
+{
+public:
+	field();
+	~field();
+	bool Start();
+	void Update();
+
+	prefab::CSkinModelRender* m_skinModelRender = nullptr;
+	//静的物理オブジェクト。
+	CPhysicsStaticObject m_phyStaticObject;
+
+	CVector3 pos = { 0.0f, -35.0f, 0.0f };
+	CVector3 scale = { 1.0f, 1.0f, 1.0f };
+};
+
