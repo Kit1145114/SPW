39b94bf510ed37291b5d0179d39a5fe7a88b94f7
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2019-05-08 17:47:31 +0900

    BHpop

--- Game/BlackHole.cpp
+++ Game/BlackHole.cpp
@@ -14,47 +14,55 @@ BlackHole::~BlackHole()
 bool BlackHole::Start()
 {
 	m_game = Game::GetInstance();
-
+	//エフェクトを作成。
+	prefab::CEffect* effect;
+	effect = NewGO<prefab::CEffect>(0);
+	//エフェクトを再生。
+	effect->Play(L"effect/BH.efk");
+	CVector3 scale = { 1.0f,1.0f,1.0f };
+	effect->SetScale(scale/**radius*/);
+	effect->SetPosition(m_position);
 	return true;
 }
 
 void BlackHole::Generate(CVector3 position, float magnification)
 {
-	
-	//エフェクトを作成。
-	prefab::CEffect* effect;
-	effect = NewGO<prefab::CEffect>(0);
-	BlackHole* bh =NewGO<BlackHole>(0, "BlackHole");
+	//ポジションの保存。
+	BlackHole* bh = NewGO<BlackHole>(0, "BlackHole");
 	bh->m_position = position;
-	//エフェクトを再生。
-	effect->Play(L"effect/BH.efk");
-	CVector3 scale = CVector3::Zero;
-	effect->SetScale(scale*magnification);
-	effect->SetPosition(position);
+	bh->radius *= magnification;
+	////エフェクトを作成。
+	//prefab::CEffect* effect;
+	//effect = NewGO<prefab::CEffect>(0);
+	////エフェクトを再生。
+	//effect->Play(L"effect/BH.efk");
+	//CVector3 scale = { 10.0f,10.0f,10.0f };
+	//effect->SetScale(scale*magnification);
+	//effect->SetPosition(position);
 }
 
 void BlackHole::Move()
 {
-	//Playerサーチ
-	//for (int i = 0; i < Game::GetInstance()->GetSansenKazu(); i++) {
-	//	CVector3 player_kyori = Game::GetInstance()->m_player[i]->GetPosition() - m_position;
-	//	if (player_kyori.Length() < radius*30) {
-	//		Game::GetInstance()->m_player[i]->SetPosition(m_position);
-	//			//Game::GetInstance()->m_player[i]->GetPosition() -(player_kyori/5));
-	//			
-	//	}
-	//}
-	//Plametサーチ
+	//Playerサーチ。
+	for (int i = 0; i < Game::GetInstance()->GetSansenKazu(); i++) {
+		CVector3 player_kyori = Game::GetInstance()->m_player[i]->GetPosition() - m_position;
+		if (player_kyori.Length() < radius) {
+			Game::GetInstance()->m_player[i]->SetPosition(m_position);
+				//Game::GetInstance()->m_player[i]->GetPosition() -(player_kyori/5));
+				
+		}
+	}
+	//Plametサーチ。
 	for (int i = 0; i < Planetnumber_Num; i++) {
 		CVector3 plamet_kyori = Game::GetInstance()->memoryPP[i]->GetPosition() - m_position;
-		if (plamet_kyori.Length() < radius * 50) {
+		if (plamet_kyori.Length() < radius) {
 			Game::GetInstance()->memoryPP[i]->SetPosition(Game::GetInstance()->memoryPP[i]->GetPosition() + (plamet_kyori*-1 / 10));
 		}
 	}
-
+	//Bulletサーチ。
 	QueryGOs<Bullet>("PlayerBullet", [&](Bullet* b) ->bool {
 		CVector3 kyori = b->GetPosition() - m_position;
-		if (kyori.Length() < radius * 50) {
+		if (kyori.Length() < radius) {
 			b->SetPosition(b->GetPosition()-(kyori / 5));
 		}
 		return true;
@@ -82,5 +90,5 @@ void BlackHole::Death()
 void BlackHole::Update()
 {
 	Move();
-	Count();
+	//Count();
 }
