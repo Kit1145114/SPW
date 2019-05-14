3c30bbaf69186e581b1569b07b926a18004a7f28
hirano <t>
2019-05-14 16:07:05 +0900

    弾の追加

--- Game/Draw_Skazu.h
+++ Game/Draw_Skazu.h
@@ -15,11 +15,11 @@ public:
 	void PostRender(CRenderContext& rc) override;
 	void AddKazu(int kazu)
 	{
-		S_kazu += kazu;
+		star += kazu;
 	}
 	void SetKazu(int kazu)
 	{
-		S_kazu = kazu;
+		star = kazu;
 	}
 	void SetS_kazuPosition(float x, float y)
 	{
@@ -42,15 +42,40 @@ public:
 	{
 		draw_S = star;
 	}
+	void AddBulletKazu(int kazu)
+	{
+		Bullet += kazu;
+	}
+	void SetBullet(const wchar_t* star)
+	{
+		draw_B = star;
+	}
+	void SetB_kazuPosition(float x, float y)
+	{
+		B_kazuposition.x = x;
+		B_kazuposition.y = y;
+	}
+	void SetBulletposition(float x, float y)
+	{
+		BulletPosition.x = x;
+		BulletPosition.y = y;
+	}
 private:
 	prefab::CSpriteRender* Star = nullptr;
+	prefab::CSpriteRender* Bullet = nullptr;
 	CFont m_font;
 	Game* m_game = nullptr;
 	Player* m_player = nullptr;
 	CVector2 S_kazuposition = CVector2::Zero;
 	CVector3 Starposition = CVector3::Zero;
+
+	CVector2 B_kazuposition = CVector2::Zero;
+	CVector3 BulletPosition = CVector3::Zero;
+
 	CVector4 Color = { 0.0f,0.0f,0.0f,0.0f };
-	int S_kazu = 0;
+	int star = 0;
+	int bullet = 0;
 	const wchar_t* draw_S = nullptr;
+	const wchar_t* draw_B = nullptr;
 };
 
